d3a09e580937826e0bb7dfac6fe10796d5afb003
StephenGregan <grego@live.ie>
2018-06-05 11:27:25 +0100

    update

--- ConsoleAppConsoleSearch/Program.cs
+++ ConsoleAppConsoleSearch/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Azure.Search;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,33 @@ namespace ConsoleAppConsoleSearch
     {
         static void Main(string[] args)
         {
+
+            string serviceName = "";
+
+            string apiKey = "";
+
+            SearchServiceClient serviceClientApi = Helper.Initialize(serviceName, apiKey);
+            ISearchIndexClient indexClientApi = serviceClientApi.Indexes.GetClient(Helper.IndexName);
+
+            Uploader uploader = new Uploader();
+            uploader.Upload(indexClientApi);
+
+            Searcher searcher = new Searcher();
+
+            Console.WriteLine("Search by name....\n");
+            searcher.SearchDocuments(indexClientApi, "NAME");
+            Console.WriteLine(string.Empty);
+
+            //Console.WriteLine("Search for status active....\n");
+            //searcher.SearchDocuments(indexClientApi, "*", "Active eq 'true'");
+            //Console.WriteLine(string.Empty);
+
+            //Console.WriteLine("Begin searching by ..... facet\n");
+            List<string> facets = new List<string>();
+            facets.Add("Category");
+            //apiKey.SearchDocuments(indexClientApi, "*", null, facets);
+
+            Console.ReadLine();
         }
     }
 }
---
+++ ConsoleAppConsoleSearch/Uploader.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Azure.Search;
+using Microsoft.Azure.Search.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleAppConsoleSearch
+{
+    class Uploader
+    {
+
+        private List<Contacts> PrepareDocuments()
+        {
+            List<Contacts> contactsDocuments = new List<Contacts>();
+
+            Contacts m1 = new Contacts
+            {
+                Id = "",
+                Name = "",
+                Active = false
+
+            };
+
+            Contacts m2 = new Contacts
+            {
+                Id = "",
+                Name = "",
+                Active = true
+
+            };
+
+            Contacts m3 = new Contacts
+            {
+                Id = "",
+                Name = "",
+                Active = false
+
+            };
+
+            Contacts m4 = new Contacts
+            {
+                Id = "",
+                Name = "",
+               Active = true
+
+            };
+
+            contactsDocuments.Add(m1);
+            contactsDocuments.Add(m2);
+            contactsDocuments.Add(m3);
+            contactsDocuments.Add(m4);
+
+            return contactsDocuments;
+
+        }
+
+        public void Upload(ISearchIndexClient indexClient)
+        {
+            try
+            {
+                var documents = PrepareDocuments();
+                var batch = IndexBatch.Upload(documents);
+                indexClient.Documents.Index(batch);
+
+                Thread.Sleep(2000);
+            }
+            catch (IndexBatchException e)
+            {
+                Console.WriteLine($"Opps The Following index failed...\n { e.IndexingResults.Where(r => !r.Succeeded).Select(r => r.Key)}");
+
+            }
+
+        }
+    }
+}
